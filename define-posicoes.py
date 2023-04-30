223b1a1de2a1c94b7f382b3607a8e6e1b804bcd4
alis-on-araujo <alisonao@al.insper.edu.br>
2023-04-30 19:48:09 -0300

    adicionei o tabuleiro

--- define-posicoes.py
+++ define-posicoes.py
@@ -1,3 +1,17 @@
+tabuleiro = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]
+
+
 def define_posicoes(linha, coluna, orient, tam): #recebe linha, coluna, orientação e o tamanho
 
     ocupados = [] #cria lista das posições ocuoadas
@@ -6,10 +20,12 @@ def define_posicoes(linha, coluna, orient, tam): #recebe linha, coluna, orienta�
 
         for i in range(tam):
             ocupados.append([linha + i, coluna])
+            tabuleiro[linha + i][coluna] += 1
 
     if orient == 'horizontal':
 
         for i in range(tam):
             ocupados.append([linha, coluna + i])
+            tabuleiro[linha][coluna + i] += 1
 
     return ocupados
--- funcoes.py
+++ funcoes.py
@@ -2,19 +2,35 @@
 
 #FUNÇÃO 01 - DEFINE POSIÇÕES
 
+tabuleiro = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]
+
+
 def define_posicoes(linha, coluna, orient, tam): #recebe linha, coluna, orientação e o tamanho
 
-    ocupados = [] #cria lista das posições ocupadas
+    ocupados = [] #cria lista das posições ocuoadas
 
     if orient == 'vertical':
 
         for i in range(tam):
             ocupados.append([linha + i, coluna])
+            tabuleiro[linha + i][coluna] += 1
 
     if orient == 'horizontal':
 
         for i in range(tam):
             ocupados.append([linha, coluna + i])
+            tabuleiro[linha][coluna + i] += 1
 
     return ocupados
 
