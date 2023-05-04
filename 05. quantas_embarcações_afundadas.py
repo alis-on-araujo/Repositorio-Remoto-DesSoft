bbb081b1a931679cf97573471244f217d2090cf5
alis-on-araujo <alisonao@al.insper.edu.br>
2023-05-03 22:43:07 -0300

    atualização ex5

--- 05. quantas_embarcações_afundadas.py
+++ 05. quantas_embarcações_afundadas.py
@@ -1,15 +1,17 @@
 def afundados(frota, tabuleiro):
     
     afundados= 0
-    for valores  in frota.values():
-        for cordenadas in valores:
-            for i , j in cordenadas:
-                if  tabuleiro[i][j]!='X':
+    
+    for posicoes in frota.values():
+
+        for coordenadas in posicoes:
+
+            for i , j in coordenadas:
+
+                if tabuleiro[i][j] !='X':
                     break 
 
             else:
                 afundados+=1
-    
-    
     
     return afundados
