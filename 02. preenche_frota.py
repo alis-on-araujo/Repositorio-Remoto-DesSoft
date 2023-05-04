87a794e9190205bd111fff034a702f7485fd9eaf
alis-on-araujo <alisonao@al.insper.edu.br>
2023-05-04 00:04:46 -0300

    Apenas estética

--- 02. preenche_frota.py
+++ 02. preenche_frota.py
@@ -6,7 +6,7 @@ def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
         frota[nome] = []
 
     posicoes = funcoes.define_posicoes(linha, coluna, orientacao, tamanho)
-    
+
     frota[nome].append(posicoes)
 
     return frota
