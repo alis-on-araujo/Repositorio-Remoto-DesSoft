97094e1fd0892a40d7511ede2d4fe2bf8f66b6b1
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2023-05-10 01:11:10 -0300

    testeeeee

--- teste.rafa.py
+++ teste.rafa.py
@@ -1,7 +1,24 @@
-linha_ataque= 1
-coluna_ataque=2
-jogadas=[[1,2]]
-if [linha_ataque, coluna_ataque] not in jogadas:
-                    jogadas.append([linha_ataque, coluna_ataque])
-
-print(jogadas)
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+tamanho_frota_oponenete=0
+for maquinas in frota_oponente.values():
+    for i in maquinas:
+        tamanho_frota_oponenete +=1
